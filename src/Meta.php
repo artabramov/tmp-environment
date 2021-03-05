d4b98c970acea9264ac3888c735df357692c5648
Artem Abramov <notdepot@gmail.com>
2021-03-05 20:02:18 +0000

    next update

--- src/Meta.php
+++ src/Meta.php
@@ -5,69 +5,72 @@ namespace artabramov\Echidna;
 class Meta
 {
     private $db;
-    private $data;
+    private $error;
+    private $id;
+    private $date;
+    private $user_id;
+    private $meta_key;
+    private $meta_value;
 
-    // create the object
+    // construct *
     public function __construct( \Illuminate\Database\Capsule\Manager $db ) {
         $this->db = $db;
         $this->clear();
     }
 
-    // clear data
-    public function clear() {
-        $this->data = [
-            'id'         => 0,
-            'date'       => '0000-00-00 00:00:00',
-            'user_id'    => 0,
-            'meta_key'   => '',
-            'meta_value' => ''
-        ];
-    }
+    // set *
+    public function __set( string $key, $value ) {}
 
-    // set the data
-    public function __set( string $key, $value ) {
-        if( array_key_exists( $key, $this->data ) ) {
-            $this->data[ $key ] = $value;
-        }
-    }
-
-    // get the data
+    // get *
     public function __get( string $key ) {
-        if( array_key_exists( $key, $this->data ) ) {
-            return $this->data[ $key ];
-        }
-        return null;
+        return isset( $this->$key ) ? $this->$key : null;
     }
 
-    // check data is not empty
-    public function is_empty( string $key ): bool {
-        if( $key == 'date' ) {
-            return $this->data[$key] == '0000-00-00 00:00:00';
-        }
-        return empty( $this->data[ $key ] );
+    // clear data
+    private function clear() {
+        $this->error      = '';
+        $this->id         = 0;
+        $this->date       = '0000-00-00 00:00:00';
+        $this->user_id    = 0;
+        $this->meta_key   = '';
+        $this->meta_value = '';
     }
 
-    // data validation
-    public function is_correct( string $key ) : bool {
+    // is error exists *
+    public function is_error() {
+        return !empty( $this->error );
+    }
 
-        if ( $key == 'id' and is_int( $this->data['id'] ) and $this->data['id'] > 0 and ceil( log10( $this->data['id'] )) <= 20 ) {
+    // is data empty
+    private function is_empty( string $key ): bool {
+
+        if( $key = 'date' )) {
+            return $this->date == '0000-00-00 00:00:00' or empty( $this->date );
+        }
+        return empty( $this->$key );
+    }
+
+    // is data correct
+    private function is_correct( string $key ) : bool {
+
+        if ( $key == 'id' and is_int( $this->id ) and $this->id > 0 and ceil( log10( $this->id )) <= 20 ) {
             return true;
 
-        } elseif ( $key == 'user_id' and is_int( $this->data['user_id'] ) and $this->data['user_id'] > 0 and ceil( log10( $this->data['user_id'] )) <= 20 ) {
+        } elseif ( $key == 'user_id' and is_int( $this->user_id ) and $this->user_id > 0 and ceil( log10( $this->user_id )) <= 20 ) {
             return true;
 
-        } elseif ( $key == 'meta_key' and is_string( $this->data['meta_key'] ) and mb_strlen( $this->data['meta_key'], 'utf-8' ) <= 40 and preg_match("/^[a-z0-9_-]/", $this->data['meta_key'] ) ) {
+        } elseif ( $key == 'meta_key' and is_string( $this->meta_key ) and mb_strlen( $this->meta_key, 'utf-8' ) <= 40 and preg_match("/^[a-z0-9_-]/", $this->meta_key ) ) {
             return true;
 
-        } elseif ( $key == 'meta_value' and is_string( $this->data['meta_value'] ) and mb_strlen( $this->data['meta_value'], 'utf-8' ) <= 255 ) {
+        } elseif ( $key == 'meta_value' and is_string( $this->meta_value ) and mb_strlen( $this->meta_value, 'utf-8' ) <= 255 ) {
             return true;
         }
 
         return false;
     }
 
-    // check that meta exists
-    public function is_exists( array $args ) : bool {
+    // is meta exists
+    private function is_exists( array $args ) : bool {
 
         $meta = $this->db
             ->table('user_meta')
@@ -81,63 +84,71 @@ class Meta
         return empty( $meta->id ) ? false : true;
     }
 
-    // insert a new usermeta
-    public function insert() : bool {
+    // insert meta
+    private function insert() : bool {
 
-        $this->data['id'] = $this->db
-        ->table('user_meta')
-        ->insertGetId([
+        $this->id = $this->db
+            ->table('user_meta')
+            ->insertGetId([
             'date'        => $this->db::raw('now()'),
-            'user_id'     => $this->data['user_id'],
-            'meta_key'    => $this->data['meta_key'],
-            'meta_value'  => $this->data['meta_value']
+            'user_id'     => $this->user_id,
+            'meta_key'    => $this->meta_key,
+            'meta_value'  => $this->meta_value 
         ]);
 
-        return empty( $this->data['id'] ) ? false : true;
+        return empty( $this->id ) ? false : true;
     }
 
-    // update
-    public function update() : bool {
+    // update meta
+    private function update() : bool {
 
         $affected_rows = $this->db
             ->table('user_meta')
-            ->where([ ['user_id', '=', $this->data['user_id']], [ 'meta_key', '=', $this->data['meta_key']] ])
-            ->update([ 'meta_value'  => $this->data['meta_value']]);
+            ->where([ ['user_id', '=', $this->user_id], [ 'meta_key', '=', $thismeta_key] ])
+            ->update([ 'meta_value'  => $this->meta_value]);
 
         return is_int( $affected_rows ) ? true : false;
     }
 
-    // select usermeta by user_id and meta_key
-    public function select() : bool {
+    // select meta
+    private function select() : bool {
 
         $meta = $this->db
             ->table( 'user_meta' )
-            ->where([[ 'user_id', '=', $this->data['user_id'] ], [ 'meta_key', '=', $this->data['meta_key'] ]])
+            ->where([[ 'user_id', '=', $this->user_id ], [ 'meta_key', '=', $this->meta_key ]])
             ->select( '*' )
             ->first();
 
         if( !empty( $meta->id )) {
-            $this->data['id'] = $meta->id;
-            $this->data['date'] = $meta->date;
-            $this->data['user_id'] = $meta->user_id;
-            $this->data['meta_key'] = $meta->meta_key;
-            $this->data['meta_value'] = $meta->meta_value;
+            $this->id         = $meta->id;
+            $this->date       = $meta->date;
+            $this->user_id    = $meta->user_id;
+            $this->meta_key   = $meta->meta_key;
+            $this->meta_value = $meta->meta_value;
         }
 
         return empty( $meta->id ) ? false : true;
     }
 
-
-    // delete usermeta by user_id and meta_key
-    public function delete() : bool {
+    // delete meta
+    private function delete() : bool {
 
         $affected_rows = $this->db
             ->table('user_meta')
-            ->where([ ['user_id', '=', $theis->data['user_id']], [ 'meta_key', '=', $this->data['meta_key'] ] ])
+            ->where([ ['user_id', '=', $this->user_id], ['meta_key', '=', $this->meta_key] ])
             ->delete();
 
         // TODO: check deleting
         return $affected_rows > 0 ? true : false;
     }
+
+    // insert/update meta *
+    public function set( int $user_id, string $meta_key, string $meta_value ) : bool {}
+
+    // select meta *
+    public function get( int $user_id, string $meta_key ) : bool {}
+
+    // delete meta *
+    public function unset( int $user_id, string $meta_key ) : bool {}
 
 }
--- src/User.php
+++ src/User.php
@@ -7,7 +7,7 @@ class User
     private $db;
     private $error;
     private $id;
-    private $data;
+    private $date;
     private $user_status;
     private $user_token;
     private $user_email;
@@ -15,13 +15,25 @@ class User
     private $user_hash;
     private $hash_date;
 
-    // create the object *
+    // construct *
     public function __construct( \Illuminate\Database\Capsule\Manager $db ) {
+        $this->db = $db;
+        $this->clear();
+    }
 
-        $this->db          = $db;
+    // set *
+    public function __set( string $key, $value ) {}
+
+    // get *
+    public function __get( string $key ) {
+        return isset( $this->$key ) ? $this->$key : null;
+    }
+
+    // clear data
+    private function clear() {
         $this->error       = '';
         $this->id          = 0;
-        $this->data        = '0000-00-00 00:00:00';
+        $this->date        = '0000-00-00 00:00:00';
         $this->user_status = '';
         $this->user_token  = '';
         $this->user_email  = '';
@@ -30,57 +42,49 @@ class User
         $this->hash_date   = '0000-00-00 00:00:00';
     }
 
-    // set the data *
-    public function __set( string $key, $value ) {}
-
-    // get the data *
-    public function __get( string $key ) {
-        return isset( $this->$key ) ? $this->key : null;
-    }
-
-    // check error exists *
+    // is error exists *
     public function is_error() {
-        return empty( $this->error );
+        return !empty( $this->error );
     }
 
-    // check data exists
+    // is data empty
     private function is_empty( string $key ): bool {
 
         if( in_array( $key, [ 'date', 'hash_date' ] )) {
-            return $this->data[$key] == '0000-00-00 00:00:00';
+            return $this->$key == '0000-00-00 00:00:00' or empty( $this->$key );
         }
-        return empty( $this->data[ $key ] );
+        return empty( $this->$key );
     }
 
-    // data validation
+    // is data correct
     private function is_correct( string $key ) : bool {
 
-        if ( $key == 'id' and is_int( $this->data['id'] ) and $this->data['id'] > 0 and ceil( log10( $this->data['id'] )) <= 20 ) {
+        if ( $key == 'id' and is_int( $this->id ) and $this->id > 0 and ceil( log10( $this->id )) <= 20 ) {
             return true;
 
-        } elseif ( $key == 'user_status' and is_string( $this->data['user_status'] ) and mb_strlen( $this->data['user_status'], 'utf-8' ) <= 40 and preg_match("/^[a-z0-9_-]/", $this->data['user_status'] ) ) {
+        } elseif ( $key == 'user_status' and is_string( $this->user_status ) and mb_strlen( $this->user_status, 'utf-8' ) <= 40 and preg_match("/^[a-z0-9_-]/", $this->user_status ) ) {
             return true;
 
-        } elseif ( $key == 'user_token' and is_string( $this->data['user_token'] ) and mb_strlen( $this->data['user_token'], 'utf-8' ) == 80 ) {
+        } elseif ( $key == 'user_token' and is_string( $this->user_token ) and mb_strlen( $this->user_token, 'utf-8' ) == 80 ) {
             return true;
 
-        } elseif ( $key == 'user_email' and is_string( $this->data['user_email'] ) and mb_strlen( $this->data['user_email'], 'utf-8' ) <= 255 and preg_match("/^[a-z0-9._-]{1,80}@(([a-z0-9-]+\.)+(com|net|org|mil|"."edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-"."9]{1,3}\.[0-9]{1,3})$/", $this->data['user_email'] ) ) {
+        } elseif ( $key == 'user_email' and is_string( $this->user_email ) and mb_strlen( $this->user_email, 'utf-8' ) <= 255 and preg_match("/^[a-z0-9._-]{1,80}@(([a-z0-9-]+\.)+(com|net|org|mil|"."edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-"."9]{1,3}\.[0-9]{1,3})$/", $this->user_email ) ) {
             return true;
 
-        } elseif ( $key == 'user_pass' and is_string( $this->data['user_pass'] ) and !empty( $this->data['user_pass'] ) ) {
+        } elseif ( $key == 'user_pass' and is_string( $this->user_pass ) and !empty( $this->user_pass ) ) {
             return true;
 
-        } elseif ( $key == 'user_hash' and is_string( $this->data['user_hash'] ) and mb_strlen( $this->data['user_hash'], 'utf-8' ) == 40 ) {
+        } elseif ( $key == 'user_hash' and is_string( $this->user_hash ) and mb_strlen( $this->user_hash, 'utf-8' ) == 40 ) {
             return true;
 
-        } elseif ( $key == 'hash_date' and is_string( $this->data['user_hash'] ) and preg_match("/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/", $this->data['hash_date'] ) ) {
+        } elseif ( $key == 'hash_date' and is_string( $this->user_hash ) and preg_match("/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/", $this->hash_date ) ) {
             return true;
         }
 
         return false;
     }
 
-    // check that the user exists
+    // is user exists
     private function is_exists( array $args ) : bool {
         $user = $this->db
         ->table('users')
@@ -92,7 +96,7 @@ class User
         return empty( $user->id ) ? false : true;
     }
 
-    // generate unique random token
+    // generate token
     private function token_create() : string {
         do {
             $user_token = bin2hex( random_bytes( 40 ));
@@ -105,7 +109,7 @@ class User
         return $user_token;
     }
 
-    // generate random password
+    // generate password
     private function pass_create() : string {
         $user_pass = '';
         for( $i = 0; $i < 8; $i++ ) {
@@ -114,76 +118,76 @@ class User
         return $user_pass;
     }
 
-    // return a hash
+    // get hash
     private function hash_create( $user_pass ) : string {
-        return sha1( $this->data['user_pass'] );
+        return sha1( $this->user_pass );
     }
 
     // insert user
-    private function user_insert() : bool {
+    private function insert() : bool {
         
-        $this->data['id'] = $this->db
+        $this->id = $this->db
         ->table('users')
         ->insertGetId([
             'date'        => $this->db::raw('now()'),
-            'user_status' => $this->data['user_status'],
-            'user_token'  => $this->data['user_token'],
-            'user_email'  => $this->data['user_email'],
-            'user_hash'   => $this->data['user_hash'],
-            'hash_date'   => $this->data['hash_date']
+            'user_status' => $this->user_status,
+            'user_token'  => $this->user_token,
+            'user_email'  => $this->user_email,
+            'user_hash'   => $this->user_hash,
+            'hash_date'   => $this->hash_date
         ]);
 
-        return empty( $this->data['id'] ) ? false : true;
+        return empty( $this->id ) ? false : true;
     }
 
     // select user
-    private function user_select( string $key ) : bool {
+    private function select( string $key ) : bool {
   
         $user = $this->db
             ->table('users')
             ->select(['*'])
-            ->where([ [ $key, '=', $this->data[ $key ] ] ])
+            ->where([ [ $key, '=', $this->$key ] ])
             ->first();
 
         if( !empty( $user->id )) {
-            $this->data['id'] = $user->id;
-            $this->data['date'] = $user->date;
-            $this->data['user_status'] = $user->user_status;
-            $this->data['user_token'] = $user->user_token;
-            $this->data['user_email'] = $user->user_email;
-            $this->data['user_hash'] = $user->user_hash;
-            $this->data['hash_date'] = $user->hash_date;
+            $this->id          = $user->id;
+            $this->date        = $user->date;
+            $this->user_status = $user->user_status;
+            $this->user_token  = $user->user_token;
+            $this->user_email  = $user->user_email;
+            $this->user_hash   = $user->user_hash;
+            $this->hash_date   = $user->hash_date;
         }
 
         return empty( $user->id ) ? false : true;
     }
 
-    // update user
-    private function user_update( array $keys ) : bool {
+    // user update
+    private function update( array $keys ) : bool {
 
         $data = [];
         foreach( $keys as $key ) {
-            $data[ $key ] = $this->data[ $key ];
+            $data[ $key ] = $this->$key;
         }        
         
         $affected_rows = $this->db
             ->table('users')
-            ->where([[ 'id', '=', $this->data['id'] ]])
+            ->where([[ 'id', '=', $this->id ]])
             ->update( $data );
 
         return is_int( $affected_rows ) ? true : false;
     }
 
-    // register *
+    // user register *
     public function register( string $user_email ) : bool {
 
-        if( $user->is_empty( 'user_email' )) {
+        if( $this->is_empty( 'user_email' )) {
             $this->error = 'user_email is empty';
         
-        } elseif( !$user->is_correct( 'user_email' )) {
+        } elseif( !$this->is_correct( 'user_email' )) {
             $this->error = 'user_email is incorrect';
         
-        } elseif( $user->is_exists( [['user_email', '=', $user->user_email]] )) {
+        } elseif( $this->is_exists( [['user_email', '=', $user->user_email]] )) {
             $this->error = 'user_email is exists';
         
         } else {
@@ -194,9 +198,34 @@ class User
             $this->user_hash   = $this->hash_create( $this->user_pass );
             $this->hash_date   = '0000-00-00 00:00:00';
 
-            if( !$user->insert() ) {
+            if( !$this->insert() ) {
+                $this->clear();
                 $this->error = 'user insert error';
             }
+        }
+
+        return empty( $this->error ) ? true : false;
+    }
+
+    // user restore *
+    public function restore( string $user_email ) : bool {
+
+        if( $this->is_empty( 'user_email' )) {
+            $this->error = 'user_email is empty';
+        
+        } elseif( !$this->is_correct( 'user_email' )) {
+            $this->error = 'user_email is incorrect';
+        
+        } elseif( !$this->is_exists( [['user_email', '=', $user->user_email], ['user_status', '<>', 'trash']] )) {
+	        $this->error = 'user_email is unavailable';
+
+        } else {
+	        $this->user_email = $user_email;
+        
+            if( !$this->select( 'user_email' )) {
+                $this->clear();
+                $this->error = 'user restore error';
+	        }
         }
     }
     
