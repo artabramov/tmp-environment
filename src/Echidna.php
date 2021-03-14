b728d4da6876d717eca6ee0565e5ad6b06e99f3f
Artem Abramov <notdepot@gmail.com>
2021-03-14 14:30:02 +0000

    datetime bug fixed

--- src/Echidna.php
+++ src/Echidna.php
@@ -69,10 +69,11 @@ class Echidna
 
     // is datetime +
     public function is_datetime( int|string $value ) : bool {
-        if( !is_string( $value ) or !preg_match("/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/", $value )) {
+        if( !is_string( $value ) or !preg_match("/^\d{4}-((0[0-9])|(1[0-2]))-(([0-2][0-9])|(3[0-1])) (([0-1][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]$/", $value )) {
             return false;
         }
-        return is_int( strtotime( $value ));
+
+        return checkdate( substr( $value, 5, 2 ), substr( $value, 8, 2 ), substr( $value, 0, 4 ));
     }
 
     // is token (a-f0-9 {80}) +
@@ -85,7 +86,7 @@ class Echidna
         return is_string( $value ) and preg_match("/^[a-f0-9]{40}$/", $value );
     }
 
-    // is email +
+    // is email +/-
     public function is_email( int|string $value ) : bool {
         return is_string( $value ) and preg_match("/^[a-z0-9._-]{2,80}@(([a-z0-9_-]+\.)+(com|net|org|mil|"."edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-"."9]{1,3}\.[0-9]{1,3})$/", $value );
     }
