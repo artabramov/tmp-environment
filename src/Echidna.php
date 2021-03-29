c3733e408cf4d1f3722eb7c464a37f74038e09b1
Artem Abramov <notdepot@gmail.com>
2021-03-29 06:45:53 +0000

    refactored the Echidna class

--- src/Echidna.php
+++ src/Echidna.php
@@ -5,51 +5,67 @@ class Echidna
 {
     protected $pdo;
     protected $e;
-
     protected $error = null;
     protected $rows = [];
 
-    // __construct
     public function __construct( \PDO $pdo ) {
         $this->pdo = $pdo;
     }
 
-    // __get
     public function __get( string $key ) {
         if( property_exists( $this, $key )) {
             return $this->$key;
         }
     }
 
-    // __isset
     public function __isset( string $key ) {
         $value = property_exists( $this, $key ) ? $this->$key : null;
         $value = is_string( $value ) ? trim( $value ) : $value;
         return !empty( $value );
     }
 
-    // is empty
+    /**
+     * Check is value empty.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_empty( int|string $value ) : bool {
         $value = is_string( $value ) ? trim( $value ) : $value;
         return empty( $value );
     }
 
-    // is id (0-9 {1,20})
+    /**
+     * Check is value numeric up to 20 signs.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_id( int|string $value ) : bool {
         return is_int( $value ) and $value >= 0;
     }
 
-    // is key (a-z0-9_- {1,20})
+    /**
+     * Check is value a correct key string up to 20 signs.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_key( int|string $value ) : bool {
         return is_string( $value ) and preg_match("/^[a-z0-9_-]{1,20}$/", $value );
     }
 
-    // is value {1,255}
+    /**
+     * Check is value a correct data string up to 255 signs.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_value( int|string $value ) : bool {
         return is_string( $value ) and mb_strlen( $value, 'UTF-8' ) <= 255;
     }
 
-    // is datetime +
+    /**
+     * Check is value a correct datetime string.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_datetime( int|string $value ) : bool {
         if( !is_string( $value ) or !preg_match("/^\d{4}-((0[0-9])|(1[0-2]))-(([0-2][0-9])|(3[0-1])) (([0-1][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]$/", $value )) {
             return false;
@@ -57,22 +73,40 @@ class Echidna
         return checkdate( substr( $value, 5, 2 ), substr( $value, 8, 2 ), substr( $value, 0, 4 ));
     }
 
-    // is token (a-f0-9 {80})
+    /**
+     * Check is value a correct token string.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_token( int|string $value ) : bool {
         return is_string( $value ) and preg_match("/^[a-f0-9]{80}$/", $value );
     }
 
-    // is hash (a-f0-9 {20})
+    /**
+     * Check is value a correct hash string.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_hash( int|string $value ) : bool {
         return is_string( $value ) and preg_match("/^[a-f0-9]{40}$/", $value );
     }
 
-    // is email +/-
+    /**
+     * Check is value a correct email string.
+     * @param int|string $value
+     * @return bool
+     */
     protected function is_email( int|string $value ) : bool {
         return is_string( $value ) and preg_match("/^[a-z0-9._-]{2,80}@(([a-z0-9_-]+\.)+(com|net|org|mil|"."edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-"."9]{1,3}\.[0-9]{1,3})$/", $value );
     }
 
-    // is exists
+    /**
+     * Check is entry exists in the table.
+     * @param string $table
+     * @param array $args
+     * @return bool
+     * @throws \PDOException
+     */
     protected function is_exists( string $table, array $args ) : bool {
 
         try {
@@ -103,7 +137,13 @@ class Echidna
         return !empty( $rows_count );
     }
 
-    // is insert
+    /**
+     * Insert a new entry in the table.
+     * @param string $table
+     * @param array $data
+     * @return int|bool
+     * @throws \PDOException
+     */
     protected function insert( string $table, array $data ) : int|bool {
 
         try {
@@ -130,7 +170,14 @@ class Echidna
         return empty( $this->e ) ? $id : false;
     }
 
-    // update
+    /**
+     * Update an entry.
+     * @param string $table
+     * @param array $args
+     * @param array $data
+     * @return int|bool
+     * @throws \PDOException
+     */
     protected function update( string $table, array $args, array $data ) : int|bool {
 
         if( empty( $table ) or empty( $args ) or empty( $data )) {
@@ -175,7 +222,15 @@ class Echidna
         return empty( $this->e ) ? $rows : false;
     }
 
-    // is select
+    /**
+     * Select an entry.
+     * @param string $table
+     * @param array $args
+     * @param int $limit
+     * @param int $offset
+     * @return array|bool
+     * @throws \PDOException
+     */
     protected function select( string $table, array $args, int $limit = 1, int $offset = 0 ) : array|bool {
   
         if( empty( $table ) or empty( $args )) {
@@ -213,7 +268,15 @@ class Echidna
         return empty( $this->e ) ? $rows : false;
     }
 
-    // is delete
+    /**
+     * Select an entry.
+     * @param string $table
+     * @param array $args
+     * @param int $limit
+     * @param int $offset
+     * @return int|bool
+     * @throws \PDOException
+     */
     protected function delete( string $table, array $args ) : int|bool {
 
         if( empty( $table ) or empty( $args )) {
@@ -251,6 +314,7 @@ class Echidna
     /**
      * Get current database time.
      * @return string
+     * @throws \PDOException
      */
     public function get_time() : string {
 
