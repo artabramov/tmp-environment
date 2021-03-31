2e115023d83b1b58c630216a79187afa0da7e002
Artem Abramov <notdepot@gmail.com>
2021-03-31 09:53:25 +0000

    draft

--- src/Utils/Validator.php
+++ src/Utils/Validator.php
@@ -4,47 +4,59 @@ namespace artabramov\Echidna\Utils;
 class Validator
 {
     /**
-     * Check is value empty.
+     * Check is value empty string or zero.
      * @param int|string $value
      * @return bool
      */
-    public static function is_empty( string $value ) {
-        return !empty( is_string( $value ) ? trim( $value ) : $value );
+    public function isEmpty( int|string $value ) : bool {
+        return empty( is_string( $value ) ? trim( $value ) : $value );
     }
 
     /**
-     * Check is value numeric up to 20 signs.
+     * Check is value integer and not zero.
      * @param int|string $value
      * @return bool
      */
-    public static function is_id( int|string $value ) : bool {
+    public static function isId( int|string $value ) : bool {
         return is_int( $value ) and $value >= 0;
     }
 
     /**
-     * Check is value a correct key string up to 20 signs.
+     * Check is value a correct key string (a-z0-9_-).
      * @param int|string $value
+     * @param int $length
      * @return bool
      */
-    public static function is_keyable( int|string $value, int $length = 20 ) : bool {
-        return is_string( $value ) and preg_match("/^[a-z0-9_-]{1,20}$/", $value );
+    public static function isKey( int|string $value, int $length ) : bool {
+        return is_string( $value ) and preg_match("/^[a-z0-9_-]{1," . $length . "}$/", $value );
     }
 
     /**
-     * Check is value a correct data string up to 255 signs.
+     * Check is value a string.
      * @param int|string $value
+     * @param int $length
      * @return bool
      */
-    public static function is_varchar( int|string $value, int $length = 255 ) : bool {
+    public static function isString( int|string $value, int $length ) : bool {
         return is_string( $value ) and mb_strlen( $value, 'UTF-8' ) <= 255;
     }
 
+    /**
+     * Check is value a correct hex string.
+     * @param int|string $value
+     * @param int $length
+     * @return bool
+     */
+    public static function isHex( int|string $value, int $length ) : bool {
+        return is_string( $value ) and preg_match("/^[a-f0-9]{" . $length . "}$/", $value );
+    }
+    
     /**
      * Check is value a correct datetime string.
      * @param int|string $value
      * @return bool
      */
-    public static function is_datetime( int|string $value ) : bool {
+    public static function isDatetime( int|string $value ) : bool {
         if( !is_string( $value ) or !preg_match("/^\d{4}-((0[0-9])|(1[0-2]))-(([0-2][0-9])|(3[0-1])) (([0-1][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]$/", $value )) {
             return false;
         }
@@ -52,30 +64,13 @@ class Validator
     }
 
     /**
-     * Check is value a correct token string.
-     * @param int|string $value
-     * @return bool
-     */
-    public static function is_token( int|string $value ) : bool {
-        return is_string( $value ) and preg_match("/^[a-f0-9]{80}$/", $value );
-    }
-
-    /**
-     * Check is value a correct hash string.
-     * @param int|string $value
-     * @return bool
-     */
-    public static function is_hash( int|string $value ) : bool {
-        return is_string( $value ) and preg_match("/^[a-f0-9]{40}$/", $value );
-    }
-
-    /**
      * Check is value a correct email string.
      * @param int|string $value
+     * @param int $length
      * @return bool
      */
-    public static function is_email( int|string $value ) : bool {
-        return is_string( $value ) and preg_match("/^[a-z0-9._-]{2,80}@(([a-z0-9_-]+\.)+(com|net|org|mil|"."edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-"."9]{1,3}\.[0-9]{1,3})$/", $value );
+    public static function isEmail( int|string $value, int $length ) : bool {
+        return is_string( $value ) and mb_strlen( $value ) <= $length and preg_match("/^[a-z0-9._-]{2,80}@(([a-z0-9_-]+\.)+(com|net|org|mil|"."edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-"."9]{1,3}\.[0-9]{1,3})$/", $value );
     }
 
 }
---
+++ tests/ValidatorTest.php
@@ -0,0 +1,234 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__.'/../src/Utils/Validator.php';
+
+class ValidatorTest extends TestCase
+{
+    private $validator;
+
+    private function call( $object, string $method , array $parameters = [] ) {
+
+        try {
+            $className = get_class($object);
+            $reflection = new \ReflectionClass($className);
+        } catch (\ReflectionException $e) {
+           throw new \Exception($e->getMessage());
+        }
+
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    protected function setUp() : void {
+        $this->validator = new \artabramov\Echidna\Utils\Validator();
+    }
+
+    protected function tearDown() : void {
+        $this->validator = null;
+    }
+
+    /**
+     * @dataProvider addIsEmpty
+     */
+    public function testIsEmpty( $value, $expected ) {
+
+        $result = $this->call( $this->validator, 'isEmpty', [ $value ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsEmpty() {
+        return [
+
+            // TRUE: various empty values
+            [ 0, true ],
+            [ 0.0, true ],
+            [ '', true ],
+            [ ' ', true ],
+            [ '0', true ],
+            [ '0 ', true ],
+            
+
+            // FALSE: various not empty values
+            [ 1, false ],
+            [ 1.0, false ],
+            [ -1, false ],
+            [ '0.0', false],
+            [ '1', false ],
+            [ '1 ', false ],
+            [ 'value', false ],
+        ];
+    }
+
+    /**
+     * @dataProvider addIsId
+     */
+    public function testIsId( $value, $expected ) {
+        $result = $this->call( $this->validator, 'isId', [ $value ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsId() {
+        return [
+
+            // TRUE: various correct values (int|string)
+            [ 0, true ],
+            [ 1, true ],
+            [ 9223372036854775807, true ],
+ 
+            // FALSE: various not correct values (int|string)
+            [ -1, false ],
+            [ '', false ],
+            [ '0', false ],
+            [ '1', false ],
+            [ '-1', false ],
+            
+        ];
+    }
+
+    /**
+     * @dataProvider addIsKey
+     */
+    public function testIsKey( $value, $length, $expected ) {
+        $result = $this->call( $this->validator, 'isKey', [ $value, $length ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsKey() {
+        return [
+
+            // TRUE: variuous correct values (int|string)
+            [ '1', 20, true ],
+            [ 'value', 20, true ],
+            [ 'value_value_value_va', 20, true ],
+            [ 'value-value-value-va', 20, true ],
+
+            // FALSE: various incorrect values (int|string)
+            [ 0, 20, false ],
+            [ 1, 20, false ],
+            [ -1, 20, false ],
+            [ 'value value value va', 20, false ],
+            [ 'value,value-value.va', 20, false ],
+            [ 'value_value_value_val', 20, false ],
+        ];
+    }
+
+    /**
+     * @dataProvider addIsString
+     */
+    public function testIsString( $value, $length, $expected ) {
+        $result = $this->call( $this->validator, 'isString', [ $value, $length ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsString() {
+        return [
+
+            // TRUE: variuous correct values (int|string)
+            [ '', 255, true ],
+            [ ' ', 255, true ],
+            [ '0', 255, true ],
+            [ '1', 255, true ],
+            [ '-1', 255, true ],
+            [ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i', 255, true ],
+
+            // FALSE: various incorrect values (int|string)
+            [ 0, 255, false ],
+            [ 1, 255, false ],
+            [ -1, 255, false ],
+            [ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in', 255, false ],
+        ];
+    }
+
+    /**
+     * @dataProvider addIsDatetime
+     */
+    public function testIsDatetime( $value, $expected ) {
+        $result = $this->call( $this->validator, 'isDatetime', [ $value ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsDatetime() {
+        return [
+
+            // TRUE: variuous correct values (int|string)
+            [ '0001-01-01 01:01:01', true ],
+            [ '2099-12-12 23:59:59', true ],
+
+            // FALSE: various incorrect values (int|string)
+            [ 0, false ],
+            [ 1, false ],
+            [ -1, false ],
+            [ '0000-00-00 00:00:00', false ],
+            [ '1970-00-00 00:00:00', false ],
+            [ '2021-13-01 00:00:00', false ],
+            [ '2021-01-32 00:00:00', false ],
+            [ '2021-01-01 25:00:00', false ],
+            [ '2021-01-01 00:60:00', false ],
+            [ '2021-01-01 00:00:60', false ],
+            [ '2021-02-29 00:00:00', false ],
+            [ '2021-02-30 00:00:00', false ],
+            [ '2021-02-31 00:00:00', false ],
+            [ '2021-04-31 00:00:00', false ],
+            [ 'yyyy-mm-dd hh:mm:ss', false ],
+        ];
+    }
+
+    /**
+     * @dataProvider addIsHex
+     */
+    public function testIsHex( $value, $length, $expected ) {
+        $result = $this->call( $this->validator, 'isHex', [ $value, $length ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsHex() {
+        return [
+
+            // TRUE: correct value (int|string)
+            [ 'da39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd80719', 80, true ],
+
+            // FALSE: various incorrect values (int|string)
+            [ 0, 80, false ],
+            [ 1, 80, false ],
+            [ -1, 80, false ],            
+            [ 'da39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd807190', 80, false ],
+            [ 'da39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd8071', 80, false ],
+            [ 'xa39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd8071', 80, false ],
+        ];
+    }
+
+    /**
+     * @dataProvider addIsEmail
+     */
+    public function testIsEmail( $value, $length, $expected ) {
+        $result = $this->call( $this->validator, 'isEmail', [ $value, $length ] );
+        $this->assertEquals( $expected, $result );
+    }
+
+    public function addIsEmail() {
+        return [
+
+            // TRUE: variuous correct values (int|string)
+            [ 'noreply@noreply.no', 255, true ],
+            [ 'noreply.1@noreply.1.no', 255, true ],
+            [ 'noreply.noreply@noreply.noreply.no', 255, true ],
+            [ 'noreply-noreply.noreply@noreply-noreply.noreply.no', 255, true ],
+            [ 'noreply_noreply.noreply@noreply_noreply.noreply.no', 255, true ],
+
+            // FALSE: various incorrect values (int|string)
+            [ 0, 255, false ],
+            [ 1, 255, false ],
+            [ -1, 255, false ],
+            [ '@noreply.no', 255, false ],
+            [ 'noreply@noreply', 255, false ],
+            [ 'noreply@noreply.nono', 255, false ],
+
+        ];
+    }
+
+}
