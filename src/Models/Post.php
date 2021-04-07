f1fff192c1589d57943189056af1f80aa170b4dd
Artem Abramov <notdepot@gmail.com>
2021-04-07 10:08:52 +0000

    class Post added

---
+++ src/Models/Post.php
@@ -0,0 +1,128 @@
+<?php
+namespace artabramov\Echidna\Models;
+use \artabramov\Echidna\Utilities\Filter;
+
+/**
+ * @implements Sequenceable
+ */
+class Post extends \artabramov\Echidna\Models\Echidna implements \artabramov\Echidna\Interfaces\Sequenceable
+{
+    private $error;
+    private $id;
+    private $date;
+    private $parent_id;
+    private $user_id;
+    private $hub_id;
+    private $post_status;
+    private $post_type;
+    private $post_content;
+
+    public function __get( string $key ) {
+        if( property_exists( $this, $key )) {
+            return $this->$key;
+        }
+        return false;
+    }
+
+    public function __isset( string $key ) {
+        if( property_exists( $this, $key )) {
+            return !empty( $this->$key );
+        }
+        return false;
+    }
+
+    public function clear() {
+        $this->e = null;
+        $this->error = null;
+        $this->id = null;
+        $this->date = null;
+        $this->parent_id = null;
+        $this->user_id = null;
+        $this->hub_id = null;
+        $this->post_status = null;
+        $this->post_type = null;
+        $this->post_content = null;
+    }
+
+    /**
+     * @param int $parent_id
+     * @param int $user_id
+     * @param int $hub_id
+     * @param string $post_status
+     * @param string $post_type
+     * @param string $post_content
+     * @param int $min_length
+     * @param int $max_length
+     * @return bool
+     */
+    public function create( int $parent_id, int $user_id, int $hub_id, string $post_status, string $post_type, string $post_content, int $min_length, int $max_length ) : bool {
+
+        $this->clear();
+
+        if( !Filter::is_empty( $user_id ) and !Filter::is_int( $parent_id )) {
+            $this->error = 'parent_id is incorrect';
+
+        } elseif( Filter::is_empty( $user_id )) {
+            $this->error = 'user_id is empty';
+
+        } elseif( !Filter::is_int( $user_id )) {
+            $this->error = 'user_id is incorrect';
+
+        } elseif( Filter::is_empty( $hub_id )) {
+            $this->error = 'hub_id is empty';
+
+        } elseif( !Filter::is_int( $hub_id )) {
+            $this->error = 'hub_id is incorrect';
+
+        } elseif( Filter::is_empty( $post_status )) {
+            $this->error = 'post_status is empty';
+
+        } elseif( !Filter::is_key( $post_status, 20 )) {
+            $this->error = 'post_status is incorrect';
+
+        } elseif( Filter::is_empty( $post_type )) {
+            $this->error = 'post_type is empty';
+
+        } elseif( !Filter::is_key( $post_type, 20 )) {
+            $this->error = 'post_type is incorrect';
+
+        } elseif( Filter::is_empty( $post_content )) {
+            $this->error = 'post_content is empty';
+
+        } elseif( !Filter::is_string( $post_content, $min_length, $max_length )) {
+            $this->error = 'post_content is incorrect';
+
+        } else {
+            $this->parent_id = $parent_id;
+            $this->user_id = $user_id;
+            $this->hub_id = $hub_id;
+            $this->post_status = $post_status;
+            $this->post_type = $post_type;
+            $this->post_content = $post_content;
+
+            $data = [
+                'parent_id'    => $this->parent_id,
+                'user_id'      => $this->user_id,
+                'hub_id'       => $this->hub_id,
+                'post_status'  => $this->post_status,
+                'post_type'    => $this->post_type,
+                'post_content' => $this->post_content,
+            ];
+
+            $this->id = $this->insert( 'posts', $data );
+
+            if( empty( $this->id )) {
+                $this->clear();
+                $this->error = 'post insert error';
+            }   
+        }
+
+        return empty( $this->error );
+    }
+
+
+
+
+    public function getone( int $post_id ) {}
+
+}
---
+++ tests/PostTest.php
@@ -0,0 +1,130 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__.'/../src/Interfaces/Sequenceable.php';
+require_once __DIR__.'/../src/Models/Echidna.php';
+require_once __DIR__.'/../src/Models/Post.php';
+require_once __DIR__.'/../src/Utilities/Filter.php';
+
+class PostTest extends TestCase
+{
+    private $pdo;
+    private $post;
+
+    /**
+     * @param $object
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+    private function callMethod( $object, string $method , array $parameters = [] ) {
+
+        try {
+            $className = get_class($object);
+            $reflection = new \ReflectionClass($className);
+        } catch (\ReflectionException $e) {
+           throw new \Exception($e->getMessage());
+        }
+
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    /**
+     * @param $object
+     * @param string $property
+     * @return mixed
+     */
+    public function getProperty( $object, $property ) {
+        $reflectedClass = new \ReflectionClass($object);
+        $reflection = $reflectedClass->getProperty($property);
+        $reflection->setAccessible(true);
+        return $reflection->getValue($object);
+    }
+
+    protected function setUp() : void {
+
+        $dsn = 'mysql:host=' . PDO_HOST . ';dbname=' . PDO_DBASE . ';charset=' . PDO_CHARSET;
+        $args = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $this->pdo = new PDO( $dsn, PDO_USER, PDO_PASS, $args );
+        $this->post = new \artabramov\Echidna\Models\Post( $this->pdo );
+    }
+
+    protected function tearDown() : void {
+        $this->pdo = null;
+        $this->post = null;
+    }
+
+    /**
+     * @dataProvider addCreate
+     */
+    public function testCreate( $parent_id, $user_id, $hub_id, $post_status, $post_type, $post_content, $expected ) {
+
+        $stmt = $this->pdo->query( "TRUNCATE TABLE " . PDO_DBASE . ".posts;" );
+
+        $result = $this->callMethod( $this->post, 'create', [ $parent_id, $user_id, $hub_id, $post_status, $post_type, $post_content, 2, 255 ] );
+        $post = [];
+
+        $post['id']           = $this->getProperty( $this->post, 'id' );
+        $post['parent_id']    = $this->getProperty( $this->post, 'parent_id' );
+        $post['user_id']      = $this->getProperty( $this->post, 'user_id' );
+        $post['hub_id']       = $this->getProperty( $this->post, 'hub_id' );
+        $post['post_status']  = $this->getProperty( $this->post, 'post_status' );
+        $post['post_type']    = $this->getProperty( $this->post, 'post_type' );
+        $post['post_content'] = $this->getProperty( $this->post, 'post_content' );
+        $post['error']        = $this->getProperty( $this->post, 'error' );
+
+        $this->assertEquals( $result, $expected );
+        if( $result ) {
+            $this->assertEquals( $post['id'],           1 );
+            $this->assertEquals( $post['parent_id'],    $parent_id );
+            $this->assertEquals( $post['user_id'],      $user_id );
+            $this->assertEquals( $post['hub_id'],       $hub_id );
+            $this->assertEquals( $post['post_status'],  $post_status );
+            $this->assertEquals( $post['post_type'],    $post_type );
+            $this->assertEquals( $post['post_content'], $post_content );
+            $this->assertEmpty( $post['error'] );
+
+        } else {
+            $this->assertEquals( $post['id'],           null );
+            $this->assertEquals( $post['parent_id'],    null );
+            $this->assertEquals( $post['user_id'],      null );
+            $this->assertEquals( $post['hub_id'],       null );
+            $this->assertEquals( $post['post_status'],  null );
+            $this->assertEquals( $post['post_type'],    null );
+            $this->assertEquals( $post['post_content'], null );
+            $this->assertNotEmpty( $post['error'] );
+        }
+    }
+
+    public function addCreate() {
+        return [
+
+            // correct cases
+            [ 1, 1, 1, 'post_status', 'post_type', 'Lorem ipsum', true ],
+            [ 0, 1, 1, 'post_status', 'post_type', 'Lorem ipsum', true ],
+            [ 1, 1, 1, 'post_status', 'post_type', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i', true ],
+
+            // incorrect cases
+            [ 1, 0, 1, 'post_status', 'post_type', 'Lorem ipsum', false ],
+            [ 1, 1, 0, 'post_status', 'post_type', 'Lorem ipsum', false ],
+            [ 1, 1, 1, '', 'post_type', 'Lorem ipsum', false ],
+            [ 1, 1, 1, 'post_status', '', 'Lorem ipsum', false ],
+            [ 1, 1, 1, 'post_status', 'post_type', '', false ],
+            [ 1, 1, 1, 'post_status', 'post_type', 'L', false ],
+            [ 1, 1, 1, 'post_status', 'post_type', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in', false ],
+
+        ];
+    }
+
+}
