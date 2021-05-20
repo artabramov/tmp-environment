9cb688abbd8a20a11c40ec21ea55cb0582414a24
Artem Abramov <notdepot@gmail.com>
2021-05-20 12:17:52 +0000

    updated Sequence class

--- src/Sequence.php
+++ src/Sequence.php
@@ -4,36 +4,33 @@ namespace artabramov\Echidna;
 class Sequence
 {
     protected $repository;
+    protected $mapper;
     public $data = [];
 
-    public function __construct( $repository ) {
+    public function __construct( $repository, $mapper ) {
         $this->error = '';
         $this->repository = $repository;
+        $this->mapper = $mapper;
     }
 
     /**
      * @return array
      */
-    public function select( string $entity_class, array $args, array $extras ) : array {
+    public function select( $entity, array $args, array $extras ) : array {
 
-        $entity = new \artabramov\Echidna\Sequence( $repository );
+        $class = new \ReflectionClass( $entity );
+        $doc = $class->getDocComment();
+        preg_match_all( '#@entity\((.*?)\)\n#s', $doc, $tmp );
+        preg_match_all( '/\s*([^=]+)=(\S+)\s*/', !empty( $tmp[1][0]) ? $tmp[1][0] : '', $tmp );
+        $params = array_combine ( $tmp[1], $tmp[2] );
 
-        $class = new \ReflectionClass( $entity_class );
-        $doc = $entity_class->getDocComment();
-        preg_match_all( '#@' . $key . '\((.*?)\)\n#s', $doc, $tmp );
-        preg_match_all( '/\s*([^=]+)=(\S+)\s*/', !empty($tmp[1][0]) ? $tmp[1][0] : '', $tmp );
-        $entity_params = array_combine ( $tmp[1], $tmp[2] );
+        $rows = $this->repository->select( ['id'], $params['table'], $args, $extras );
 
-        $this->data = $this->repository->select( $entity_params['table'], $table, $args, $extras );
-
-        /*
         foreach( $rows as $row ) {
-            $user = new \App\Entities\User;
-            $instance = new $model( $this->pdo );
-            $instance->getone( intval( $row['id'] ));
+            $instance = clone $entity;
+            $this->mapper->select( $instance, [['id', '=', $row->id]] );
             array_push( $this->data, $instance );
         }
-        */
 
         return $this->data;
     }
