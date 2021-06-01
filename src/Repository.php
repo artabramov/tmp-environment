b88e9ea3641d7fab5cb3a8f1206dc4aa902b3fb8
Artem Abramov <notdepot@gmail.com>
2021-06-01 05:32:13 +0000

    upgrades of repository

--- src/Repository.php
+++ src/Repository.php
@@ -5,15 +5,10 @@ class Repository
 {
     protected $e;
     protected $pdo;
-    //protected $query;
-    //protected $rows;
     protected $stmt;
 
     public function __construct( $pdo ) {
-        $this->e = null;
         $this->pdo = $pdo;
-        //$this->query = new \artabramov\Echidna\Query();
-        //$this->rows = [];
     }
 
     public function __get( $key ) {
@@ -30,6 +25,9 @@ class Repository
         return false;
     }
 
+    /**
+     * Create query string from array.
+     */
     private function where( array $kwargs ) : string {
 
         return implode( ' AND ', array_map( 
@@ -42,6 +40,9 @@ class Repository
             $kwargs ));
     }
 
+    /**
+     * Create query params from array.
+     */
     private function params( array $kwargs ) : array {
 
         $params = [];
@@ -62,7 +63,9 @@ class Repository
         return $params;
     }
 
-    // return query object
+    /**
+     * Create select query.
+     */
     public function select( array $columns, string $table, array $kwargs, array $args = [] ) : \artabramov\Echidna\Query {
 
         $select = implode( ', ', $columns );
@@ -76,9 +79,9 @@ class Repository
     }
 
     /**
-     *
+     * Create insert query.
      */
-    public function insert( string $table, array $data ) {
+    public function insert( string $table, array $data ) : \artabramov\Echidna\Query {
 
         $columns = implode( ', ', array_keys( $data ));
         $keys = implode( ', ', array_fill( 0, count( $data ), '?' ));
@@ -91,9 +94,9 @@ class Repository
     }
 
     /**
-     *
+     * Create update query.
      */
-    public function update( string $table, array $args, array $data ) {
+    public function update( string $table, array $args, array $data ) : \artabramov\Echidna\Query {
 
         $set = implode( ', ', array_map( fn( $value ) => $value . ' = ?', array_keys( $data )));
         $where = $this->where( $args );
@@ -105,7 +108,10 @@ class Repository
         return $query;
     }
 
-    public function delete( string $table, array $args ) {
+    /**
+     * Create delete query.
+     */
+    public function delete( string $table, array $args ) : \artabramov\Echidna\Query {
 
         $where = $this->where( $args );
         $params = $this->params( $args );
@@ -116,8 +122,10 @@ class Repository
         return $query;
     }
 
-    // return query object
-    public function custom( string $query_text, array $query_args ) {
+    /**
+     * Create custom query.
+     */
+    public function custom( string $query_text, array $query_args ) : \artabramov\Echidna\Query {
 
         $query = new \artabramov\Echidna\Query();
         $query->text = $query_text;
@@ -125,7 +133,9 @@ class Repository
         return $query;
     }
 
-    // only execute
+    /**
+     * Execute the query.
+     */
     public function execute( $query ) {
         $this->e = null;
 
@@ -140,7 +150,9 @@ class Repository
         return empty( $this->e ) ? true : false;
     }
 
-    // execute and select rows
+    /**
+     * Select rows after query execute.
+     */
     public function rows() {
         $this->e = null;
 
@@ -154,7 +166,9 @@ class Repository
         return empty( $this->e ) ? $rows : [];
     }
 
-    // get last insert id
+    /**
+     * Get last insert id after last query execution.
+     */
     public function id() {
         $this->e = null;
 
@@ -168,133 +182,12 @@ class Repository
         return empty( $this->e ) ? $id : 0;
     }
 
-
-
-
-
     /**
-     * @return string
-     */
-    protected function _get_where( array $args ) : string {
-        return implode( ' AND ', array_map( fn( $value ) => !is_array( $value[2] ) ? $value[0] . ' ' . $value[1] . ' ?' : $value[0] . ' ' . $value[1] . ' (' . implode( ', ', array_map( fn() => '?', $value[2] ) ) . ')', $args ));
-    }
-
-    /**
-     * @return array
-     */
-    protected function _get_params( array $args ) : array {
-
-        $params = [];
-        foreach( $args as $arg ) {
-            if( is_array( $arg[2] )) {
-                foreach( $arg[2] as $param ) {
-                    $params[] = $param;
-                }
-            } else {
-                $params[] = $arg[2];
-            }
-        }
-
-        return $params;
-    }
-
-    /**
-     * @return integer
-     * @throws \Exception
-     */
-    public function _insert( string $table, array $data ) : int {
-
-        $columns = implode( ', ', array_keys( $data ));
-        $values = implode( ', ', array_fill( 0, count( $data ), '?' ));
-
-        try {
-            $stmt = $this->pdo->prepare( 'INSERT INTO ' . $table . ' ( ' . $columns . ' ) VALUES ( ' . $values . ' )' );
-            $stmt->execute( array_values( $data ));
-            $id = $this->pdo->lastInsertId();
-
-        } catch( \Exception $e ) {
-            $this->e = $e;
-        }
-
-        return empty( $this->e ) ? $id : 0;
-    }
-
-    /**
-     * @return bool
-     * @throws \Exception
-     */
-    public function _update( string $table, array $args, array $data ) : bool {
-
-        $set = implode( ', ', array_map( fn( $value ) => $value . ' = ?', array_keys( $data )));
-        $where = $this->get_where( $args );
-        $params = array_merge( array_values( $data ), $this->get_params( $args ));
-
-        try {
-            $stmt = $this->pdo->prepare( 'UPDATE ' . $table . ' SET ' . $set . ' WHERE ' . $where . ' LIMIT 1' );
-            $stmt->execute( $params );
-            $rows = $stmt->rowCount();
-
-        } catch( \Exception $e ) {
-            $this->e = $e;
-        }
-
-        return empty( $this->e ) ? true : false;
-    }
-
-    /**
-     * @return array
-     * @throws \Exception
-     */
-    public function _select( array $columns, string $table, array $args, array $extras = [] ) : array {
-
-        $select = implode( ', ', $columns );
-        $where = $this->get_where( $args );
-        $params = $this->get_params( $args );
-
-        try {
-            $sql = 'SELECT ' . $select . ' FROM ' . $table . ' WHERE ' . $where;
-
-            foreach( $extras as $key => $value ) {
-                $sql .= ' ' . $key . ' ' . $value;
-            }
-
-            $stmt = $this->pdo->prepare( $sql );
-            $stmt->execute( $params );
-            $rows = $stmt->fetchAll( $this->pdo::FETCH_OBJ );
-
-        } catch( \Exception $e ) {
-            $this->e = $e;
-        }
-
-        return empty( $this->e ) ? $rows : [];
-    }
-
-    /**
-     * @return bool
-     * @throws \Exception
-     */
-    public function _delete( string $table, array $args ) : bool {
-
-        $where = $this->get_where( $args );
-        $params = $this->get_params( $args );
-
-        try {
-            $stmt = $this->pdo->prepare( 'DELETE FROM ' . $table . ' WHERE ' . $where );
-            $stmt->execute( $params );
-            $rows = $stmt->rowCount();
-
-        } catch( \Exception $e ) {
-            $this->e = $e;
-        }
-
-        return empty( $this->e ) ? true : false;
-    }
-
-    /**
+     * Get current inner time of repository.
      * @return string
      * @throws \Exception
      */
-    public function _time() {
+    public function time() {
 
         try {
             $stmt = $this->pdo->prepare( 'SELECT NOW() AS datetime;' );
@@ -308,18 +201,6 @@ class Repository
         return empty( $this->e ) ? $rows[ 'datetime' ] : '0000-00-00 00:00:00';
     }
 
-    public function _query( string $sql, array $params ) {
 
-        try {
-            $stmt = $this->pdo->prepare( $sql );
-            $stmt->execute( $params );
-            $rows = $stmt->fetchAll( $this->pdo::FETCH_OBJ );
-
-        } catch( \Exception $e ) {
-            $this->e = $e;
-        }
-
-        return empty( $this->e ) ? $rows : [];
-    }
 
 }
