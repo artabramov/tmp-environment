4454197c9ddf712a8659d852b04b44036259e737
Artem Abramov <notdepot@gmail.com>
2022-02-04 00:02:17 +0300

    added mysql logs

--- start.sh
+++ start.sh
@@ -4,3 +4,14 @@
 docker build -t app ./app/
 docker build -t app/nginx ./nginx/
 docker-compose up -d
+
+docker exec -d mysql touch /var/log/mysql/mysql.log
+docker exec -d mysql chown mysql:mysql /var/log/mysql/mysql.log
+docker exec -d mysql mysql -u root -padmin -e "SET GLOBAL general_log='ON';"
+docker exec -d mysql mysql -u root -padmin -e "SET GLOBAL general_log_file='/var/log/mysql/mysql.log';"
+docker exec -d mysql touch /var/log/mysql/mysql-slow.log
+docker exec -d mysql chown mysql:mysql /var/log/mysql/mysql-slow.log
+docker exec -d mysql mysql -u root -padmin -e "SET GLOBAL slow_query_log=1;"
+docker exec -d mysql mysql -u root -padmin -e "SET GLOBAL slow_query_log_file='/var/log/mysql/mysql-slow.log';"
+docker exec -d mysql mysql -u root -padmin -e "SET GLOBAL long_query_time=1;"
+docker exec -d mysql mysql -u root -padmin -e "SET GLOBAL log_queries_not_using_indexes=0;"
